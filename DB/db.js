a6d79d35c5ad39e9831157b547ba480b49726551
Santhosh P <151363577+Santhosh221155@users.noreply.github.com>
2025-05-30 21:57:25 +0530

    Update db.js

--- DB/db.js
+++ DB/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/taskTimer", {
+    await mongoose.connect(MONGO_URI, {
     });
     console.log("Connected to MongoDB");
   } catch (error) {
